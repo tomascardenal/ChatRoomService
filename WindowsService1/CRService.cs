5f47be617b6febfede6905211919276ddbb71ade
Tomás <tomas.car.guitv@gmail.com>
2019-02-24 22:26:58 +0100

    Server from my chatroomServer updated to act as a service

--- WindowsService1/CRService.cs
+++ WindowsService1/CRService.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WindowsService1
@@ -16,7 +20,28 @@ namespace WindowsService1
     public partial class CRService : ServiceBase
     {
         /// <summary>
-        /// Default constructor
+        /// The default port
+        /// </summary>
+        public const int DEFAULT_PORT = 1337;
+        /// <summary>
+        /// Path to the configuration
+        /// </summary>
+        private string configPath;
+        /// <summary>
+        /// Timestamp for calculating how much time this service ran
+        /// </summary>
+        private DateTime onStartTimestamp;
+        /// <summary>
+        /// The port this service is on
+        /// </summary>
+        private int currentPort;
+        /// <summary>
+        /// Thread containing the server
+        /// </summary>
+        private Thread serverThread;
+
+        /// <summary>
+        /// Default constructor for this service
         /// </summary>
         public CRService()
         {
@@ -29,6 +54,59 @@ namespace WindowsService1
         /// <param name="args">The arguments for the startup</param>
         protected override void OnStart(string[] args)
         {
+            this.onStartTimestamp = DateTime.Now;
+            this.writeEvent("Chatroom service started, trying to load on config port");
+            this.configPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData, Environment.SpecialFolderOption.Create);
+            this.configPath = Path.Combine(this.configPath, "config.cfg");
+            try
+            {
+                using (StreamReader reader = new StreamReader(configPath))
+                {
+                    string cfg = reader.ReadLine();
+                    string port;
+                    if (cfg.Contains("Port="))
+                    {
+                        port = cfg.Substring(5);
+                        if (int.TryParse(port, out this.currentPort)){
+                            this.writeEvent("Port sucessfully loaded from cfg file: " + currentPort);
+                        }
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                this.writeEvent("Config file not found, trying to load on default port");
+                this.currentPort = DEFAULT_PORT;
+            }
+
+            if (this.currentPort != DEFAULT_PORT)
+            {
+                ChatServer server = new ChatServer(currentPort);
+                if (server.BindServer())
+                {
+                    server.RunServer();
+                }
+                else
+                {
+                    this.writeEvent("Failed to connect on cfg port, trying default port");
+                    this.currentPort = DEFAULT_PORT;
+                }
+            }
+            if(this.currentPort == DEFAULT_PORT)
+            {
+                ChatServer server = new ChatServer(currentPort);
+                if (server.BindServer())
+                {
+                    server.RunServer();
+                }
+                else
+                {
+                    this.writeEvent("Failed to connect on default port, ending service...");
+                    this.Stop();
+                }
+            }
+
+
         }
 
         /// <summary>
@@ -36,6 +114,8 @@ namespace WindowsService1
         /// </summary>
         protected override void OnStop()
         {
+            TimeSpan elapsedTimestamp = DateTime.Now - onStartTimestamp;
+            this.writeEvent("Service stopped, ran for " + elapsedTimestamp.TotalSeconds + "s");
         }
 
         /// <summary>
---
+++ WindowsService1/ChatServer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    /// <summary>
+    /// Represents a multithreaded chat server
+    /// </summary>
+    class ChatServer
+    {
+        /// <summary>
+        /// The server endpoint
+        /// </summary>
+        private IPEndPoint serverIE;
+        /// <summary>
+        /// The server socket
+        /// </summary>
+        private Socket serverSocket;
+        /// <summary>
+        /// The server port
+        /// </summary>
+        private int serverPort;
+        /// <summary>
+        /// Connected user list
+        /// </summary>
+        public Dictionary<string, Socket> ClientList;
+        /// <summary>
+        /// Lock for multithreading
+        /// </summary>
+        public object Locker = new object();
+        /// <summary>
+        /// The service controlling this server
+        /// </summary>
+        public CRService Service;
+        /// <summary>
+        /// Controls when to run the server
+        /// </summary>
+        public bool Run = true;
+
+        /// <summary>
+        /// Initalizes a chatserver on the indicated port
+        /// </summary>
+        /// <param name="port"></param>
+        public ChatServer(int port)
+        {
+            this.serverPort = port;
+            ClientList = new Dictionary<string, Socket>();
+        }
+
+        public bool BindServer()
+        {
+            Service.writeEvent(string.Format(Properties.strings.SRV_WELCOME, this.serverPort));
+            serverIE = new IPEndPoint(IPAddress.Any, this.serverPort);
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                serverSocket.Bind(serverIE);
+            }
+            catch (SocketException e)
+            {
+                Service.writeEvent(string.Format(Properties.strings.SRV_SOCKETEXCEPTION, e.Message));
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Runs the server and starts waiting for clients
+        /// </summary>
+        public void RunServer()
+        {
+            try
+            {
+                serverSocket.Listen(10);
+                //Wait for clients, throw a new thread with the client
+                while (Run)
+                {
+                    Socket client = serverSocket.Accept();
+                    ChatRoomServiceManager chatClient = new ChatRoomServiceManager(client, this);
+                    Thread clientThread = new Thread(chatClient.run);
+                    clientThread.IsBackground = true;
+                    clientThread.Start();
+                }
+            }
+            catch (SocketException e)
+            {
+                Service.writeEvent(string.Format(Properties.strings.SRV_SOCKETEXCEPTION, e.Message));
+            }
+        }
+
+
+    }
+}
